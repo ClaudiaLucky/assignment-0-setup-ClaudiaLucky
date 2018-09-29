231ec2c1be675f4af204050bf8e5e5ee493ddbbd
claudialucky <yunzizhang2019@u.northwestern.edu>
2018-09-29 11:08:28 -0500

    I haven't modified the main.py but I modified student_code.py

--- student_code.py
+++ student_code.py
@@ -1,2 +1,16 @@
+#!/usr/bin/python
+import math
+
+
 def hello_world():
-	return False
+    return True
+
+
+def circumference(diameter):
+    c = diameter * math.pi
+    return c
+
+
+def area(radius):
+    a = math.pi * pow(radius, 2)
+    return a
